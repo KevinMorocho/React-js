88a689a16280e968521f46cc454cdb08822b9196
Kevin Morocho <fabrialmeida1796@gmail.com>
2024-02-08 17:51:20 -0500

    Initialice by KevinDev

---
+++ app.js
@@ -0,0 +1,5 @@
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+const appDomElement = document.getElementById('app')
+
+const root = ReactDOM.createRoot(appDomElement)
+root.render('Aprendiendo React Otra vez')
